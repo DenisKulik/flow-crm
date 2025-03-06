e59d2e9f65f7ff4d52abecf1a1b1b98d662b5b67
dkulik <kulikdenis1996x@gmail.com>
2025-03-06 15:52:58 +0700

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section class="h-screen p-6 bg-black">
+    <div class="grid grid-cols-[100px_1fr] h-full bg-white/10 backdrop-blur-xl shadow-2xl shadow-black/50 rounded-3xl">
+      <div>Sibebar</div>
+      <div>
+        <slot />
+      </div>
+    </div>
+  </section>
+</template>
