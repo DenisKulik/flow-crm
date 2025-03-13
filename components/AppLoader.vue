c868647bde70b1a7cefe0959569c07998e47f6ca
dkulik <kulikdenis1996x@gmail.com>
2025-03-13 17:52:59 +0700

    feat: add loader component

---
+++ components/AppLoader.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="app-loader">
+    <img src="/loader.svg" alt="loader" width="100" />
+  </div>
+</template>
+
+<style scoped>
+.app-loader {
+  @apply flex items-center justify-center w-screen h-screen;
+}
+</style>
