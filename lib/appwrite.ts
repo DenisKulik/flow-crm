4c23b321bb250786ebea1d2855f0735d419f9d7b
dkulik <kulikdenis1996x@gmail.com>
2025-03-17 16:39:18 +0700

    chore: configure appwrite

---
+++ lib/appwrite.ts
@@ -0,0 +1,11 @@
+import { Client, Account, Databases, Storage } from 'appwrite'
+import { APP_WRITE_ID } from '~/constants'
+
+export const client = new Client()
+
+client.setEndpoint('https://cloud.appwrite.io/v1').setProject(APP_WRITE_ID)
+
+export const account = new Account(client)
+export { ID } from 'appwrite'
+export const DB = new Databases(client)
+export const storage = new Storage(client)
